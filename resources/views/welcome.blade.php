46d0953905584e06b47462885087871c89b950ea
Vincen <vincenhayate123@gmail.com>
2024-10-04 15:06:42 +0700

    Remove unnecessary

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,5 @@
 @section('title', 'Welcome')
 
 @section('content')
-<div class="flex gap-4 justify-center items-center h-screen overflow-auto whitespace-nowrap">
-    @include('component.product-card', with([
-        'itemName' => 'Item 1',
-        'itemPrice' => 'Rp 5.000',
-        'itemSeller' => 'Eco Seller',
-        'itemRating' => '4.5',
-        'itemReview' => '10'
-    ]))
-</div>
+
 @endsection
