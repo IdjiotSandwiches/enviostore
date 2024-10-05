ca9cea056e76f2f902ae8b9503fd0e5303f9030a
Vincen <vincenhayate123@gmail.com>
2024-10-05 22:23:11 +0700

    Create custom RegisterRequest

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\RegisterRequest;
 
 class RegisterController extends Controller
 {
     public function index()
     {
         return view('register');
+    }
+
+    public function register(RegisterRequest $registerRequest)
+    {
+        $validated = $registerRequest->validated();
+        dd($validated);
     }
 }
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -18,9 +18,14 @@
             <input type="tel" name="phone_number" id="phone-number" minlength="8" maxlength="12" pattern="[0-9]{8,12}">
         </div>
         <div class="flex flex-col">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password-confirmation">
+        </div>
+        <div class="flex flex-col">
             <label for="password_confirmation">Password Confirmation</label>
             <input type="password" name="password_confirmation" id="password-confirmation">
         </div>
+        <button class="bg-gray-100 py-2 w-full">Register</button>
     </form>
 </section>
 @endsection
