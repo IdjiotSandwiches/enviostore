88b8df76f800506553d77950be09d387642d76e9
Vincen <vincenhayate123@gmail.com>
2024-10-05 22:12:02 +0700

    Create register layout

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,5 +2,25 @@
 @section('title', 'Register')
 
 @section('content')
-    <p>Register</p>
+<section class="flex justify-center items-center">
+    <form action="POST" class="max-w-screen-md w-full">
+        @csrf
+        <div class="flex flex-col">
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="flex flex-col">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email">
+        </div>
+        <div class="flex flex-col">
+            <label for="phone_number">Phone Number</label>
+            <input type="tel" name="phone_number" id="phone-number" minlength="8" maxlength="12" pattern="[0-9]{8,12}">
+        </div>
+        <div class="flex flex-col">
+            <label for="password_confirmation">Password Confirmation</label>
+            <input type="password" name="password_confirmation" id="password-confirmation">
+        </div>
+    </form>
+</section>
 @endsection
