8f6a51fb7a8e8faaf869c1934ccb5be2e32bb022
Vincen <vincenhayate123@gmail.com>
2024-10-07 09:59:10 +0700

    Change users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('phone_number');
             $table->text('address')->nullable();
             $table->boolean('user_type')->default(0);
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
             $table->timestamps();
         });
     }
