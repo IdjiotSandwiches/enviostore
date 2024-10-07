6417b9eda0646bd6c730938e605d154df5a033cd
Vincen <vincenhayate123@gmail.com>
2024-10-07 19:45:29 +0700

    Fix submenu

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,7 +1,7 @@
 <div id="search-bar" class="hidden fixed bg-white z-50 h-screen w-screen p-4">
-    <div class="flex gap-1 items-center justify-between">
+    <div class="flex items-center justify-between">
         <div id="back-btn">
-            <svg class="w-10 h-10 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 20 24">
+            <svg class="w-10 h-10 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="5 0 20 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M5 12h10M5 12l4-4m-4 4 4 4"/>
             </svg>
         </div>
@@ -12,13 +12,13 @@
 </div>
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900 z-20">
-    <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4 gap-4">
+    <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4 gap-2 md:gap-4">
         <!-- Logo -->
         <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <span class="self-center text-xl md:text-2xl font-semibold whitespace-nowrap dark:text-white italic">EnviroStore</span>
         </a>
         <!-- Search -->
-        <div class="flex md:order-1 w-full md:w-8/12 lg:w-7/12 items-center justify-end md:justify-center gap-4">
+        <div class="flex md:order-1 w-full md:w-8/12 lg:w-7/12 items-center justify-end md:justify-center gap-2 md:gap-4">
             <!-- Search Icon -->
             <div id="search-icon" class=" md:hidden cursor-pointer select-none">
                 <svg class="w-5 h-5 md:w-6 md:h-6 text-gray-900 dark:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -31,10 +31,11 @@
             <div class="hidden md:block relative md:w-full">
                 @include('component.search-bar')
             </div>
+
             <!-- Shopping Category -->
-            <a href="/" class="hidden lg:block">
+            <a href="/" class="">
                 <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                  copath stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
                 </svg>
             </a>
         </div>
@@ -72,10 +73,10 @@
         </div>
     </div>
 </nav>
-<!-- <nav class="bg-gray-50 dark:bg-gray-700">
+<nav class="bg-gray-50 dark:bg-gray-700 overflow-auto">
     <div class="max-w-screen-xl px-4 py-3 mx-auto">
         <div class="flex items-center">
-            <ul class="flex flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-md">
+            <ul class="flex text-nowrap flex-row font-medium mt-0 space-x-8 rtl:space-x-reverse text-sm md:text-md">
                 <li>
                     <a href="#" class="text-gray-900 dark:text-white hover:underline">Category</a>
                 </li>
@@ -91,7 +92,7 @@
             </ul>
         </div>
     </div>
-</nav> -->
+</nav>
 
 @section('extra-js')
     <script>
