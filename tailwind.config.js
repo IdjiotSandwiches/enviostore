0763f10d16f071905e1aa7a306f813f37f3bd21a
Vincen <vincenhayate123@gmail.com>
2024-10-14 10:48:00 +0700

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,8 +12,9 @@ export default {
         'secondary': ['Georgia', 'sans-serif'],
     },
     colors: {
-        'primary': '',
-        'secondary': '',
+        'primary': '#ffffff',
+        'secondary': '#2c2c2c',
+        'accent': '#6f6f6f',
         'background': '#F8F8F8',
         'button': '#000000'
     },
