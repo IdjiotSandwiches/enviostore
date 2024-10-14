9241dcaae3c7cbd17fb3b0165d6f866840ed6996
Vincen <vincenhayate123@gmail.com>
2024-10-14 11:53:13 +0700

    Add error text & border for login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,11 +9,23 @@
         <h5 class="text-3xl text-center font-bold text-gray-900 dark:text-white">Login</h5>
         <div>
             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-            <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="user@email.com" required />
+            <input type="email" name="email" id="email" @class([
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white",
+                "border-red-500" => $errors->has('email')
+            ]) placeholder="user@email.com" required />
+            @error('email')
+                <p class="text-red-500 text-sm">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-            <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+            <input type="password" name="password" id="password" @class([
+                "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white",
+                "border-red-500" => $errors->has('password')
+            ]) required />
+            @error('password')
+                <p class="text-red-500 text-sm">{{ $message }}</p>
+            @enderror
         </div>
         <div class="flex items-start">
             <a href="#" class="text-sm text-font_secondary hover:text-font_primary hover:underline dark:text-font_secondary">Lost Password?</a>
