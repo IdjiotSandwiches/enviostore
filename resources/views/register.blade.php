ea804d1d9a8e5da5a66ffe3d8d30a1097544b2f3
Vincen <vincenhayate123@gmail.com>
2024-10-14 10:56:40 +0700

    Add coloring scheme for register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -28,12 +28,12 @@
             <input type="password" name="password_confirmation" id="password-confirmation" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
         </div>
         <div class="flex items-center mb-4">
-            <input id="checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+            <input id="checkbox" type="checkbox" value="" class="w-4 h-4 text-button bg-gray-100 border-gray-300 rounded focus:ring-button/15 dark:focus:ring-button dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" required>
             <label for="checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
                 Agree to the <a href="#" class="underline">terms and condition</a>
             </label>
         </div>
-        <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
+        <button type="submit" class="w-full text-white bg-button hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-button/15 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-button dark:hover:bg-button/80 dark:focus:ring-button/15">Sign Up</button>
         <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
             Already have an account? <a href="{{ route('login') }}" class="text-blue-700 hover:underline dark:text-blue-500">Login</a>
         </div>
