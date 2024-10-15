51ef3b21295e774cdbc8c09dd7f2d583eebb83c5
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-10-15 09:54:23 +0700

    Tailwind config (#12)
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ export default {
     "./node_modules/flowbite/**/*.js",
   ],
   theme: {
+    fontFamily: {
+        'primary': ['Roboto', 'sans-serif'],
+        'secondary': ['Georgia', 'sans-serif'],
+    },
+    colors: {
+        'primary': '#ffffff',
+        'accent': '#6f6f6f',
+        'background': '#F8F8F8',
+        'button': '#000000',
+        'font_primary': '#2c2c2c',
+        'font_secondary': '#6f6f6f',
+    },
     extend: {},
   },
   plugins: [
