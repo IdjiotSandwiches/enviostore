10d4678712edddadc11e59ccc27d42d45d2059df
Vincen <vincenhayate123@gmail.com>
2024-10-20 17:32:26 +0700

    Add accordion

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -46,8 +46,8 @@ $imgPaths = [
                 <h3 class="font-bold text-xl md:text-3xl">Rp 5.000</h3>
             </div>
             <div class="grid gap-4">
-                <div class="flex items-center justify-between">
-                    <label for="quantity-input" class="block mb-2 text-lg font-medium text-gray-900 dark:text-white">Amount:</label>
+                <div class="flex items-end justify-between">
+                    <label for="quantity-input" class="block mb-2 text-lg font-medium text-accent dark:text-white">Amount:</label>
                     <div class="relative flex items-center max-w-[8rem]">
                         <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-11 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                             <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
@@ -63,14 +63,33 @@ $imgPaths = [
                     </div>
                 </div>
                 <div class="grid gap-4">
-                    <a href="#" class="text-center py-2 px-5 text-lg font-medium text-gray-900 focus:outline-none rounded-lg border border-button hover:bg-accent/10 hover:text-button focus:z-10 focus:ring-4 focus:ring-button/15 dark:focus:ring-button/15 dark:text-button dark:border-button dark:hover:text-white dark:hover:bg-accent/10 text-nowrap">+ Add to Cart</a>
+                    <a href="#" class="flex items-center justify-center gap-2 text-center py-2 px-5 text-lg font-medium text-gray-900 focus:outline-none rounded-lg border border-button hover:bg-accent/10 hover:text-button focus:z-10 focus:ring-4 focus:ring-button/15 dark:focus:ring-button/15 dark:text-button dark:border-button dark:hover:text-white dark:hover:bg-accent/10 text-nowrap">
+                        <svg class="w-8 h-8 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"/>
+                        </svg>
+                        + Add to Cart
+                    </a>
                     <a href="#" class="text-white text-center bg-button hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-button/15 font-medium rounded-lg text-lg px-4 md:px-5 py-1.5 md:py-2 dark:bg-button dark:hover:bg-button/80 dark:focus:ring-button/15 text-nowrap">Buy</a>
                 </div>
             </div>
         </div>
     </section>
-    <section>
-
+    <section class="mt-10">
+        <div id="accordion-collapse" data-accordion="collapse">
+            <h2 id="accordion-collapse-heading-1">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-bold rtl:text-right text-font_primary border border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                    <span>About this Products</span>
+                    <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                    </svg>
+                </button>
+            </h2>
+            <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
+                <div class="p-5 border rounded-b-xl border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium et, magnam nulla quidem, modi voluptate ad temporibus vitae maxime alias sint praesentium delectus molestiae sunt accusantium, suscipit voluptatem mollitia.</p>
+                </div>
+            </div>
+        </div>
     </section>
 </section>
 @endsection
