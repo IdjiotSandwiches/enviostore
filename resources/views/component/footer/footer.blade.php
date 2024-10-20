750bad66fbd64b24ac4fc2e4b83059564c76cd31
appleaze <theo.harjanto@binus.ac.id>
2024-10-20 16:05:45 +0700

    fix responsive views

--- resources/views/component/footer/footer.blade.php
+++ resources/views/component/footer/footer.blade.php
@@ -1,57 +1,56 @@
 <footer class="bg-white dark:bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  {{-- Logo --}}
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white italic">EnvioStore</span>
-              </a>
-          </div>
-          {{-- Content --}}
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-            {{-- Resources --}}
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
-                  <ul class="text-gray-900 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline">Sustainable Development Goals</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">Supply Chain Transparency</a>
-                      </li>
-                  </ul>
-              </div>
-              {{-- Legal and Notices --}}
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal and Notices</h2>
-                  <ul class="text-gray-900 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="# class="hover:underline ">Privacy Policy</a>
-                      </li>
-                      <li>
-                          <a href="# class="hover:underline">Terms and Conditions</a>
-                      </li>
-                  </ul>
-              </div>
-              {{-- Media Sosial --}}
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Social Media</h2>
-                  <ul class="text-gray-900 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline">Github</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">X(Twitter)</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-      {{-- Copy Right --}}
-      <hr class="my-6 border-gray-900 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-900 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">EnviroStore</a>. All Rights Reserved.
-          </span>
-      </div>
+        <div class="flex flex-col md:flex-row md:justify-between">
+            <div class="mb-6 md:mb-0">
+                <a href="#" class="flex items-center">
+                    {{-- Logo --}}
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white italic md:pl-5">EnvioStore</span>
+                </a>
+            </div>
+            {{-- Content --}}
+            <div class="grid grid-cols-1 gap-8 sm:gap-6 sm:grid-cols-2 md:grid-cols-3 text-sm md:pl-8">
+                {{-- Resources --}}
+                <div>
+                    <h2 class="mb-4 text-md font-semibold text-gray-900 uppercase dark:text-white">Resources</h2>
+                    <ul class="text-gray-900 dark:text-gray-400 font-medium">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Sustainable Development Goals</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Supply Chain Transparency</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- Legal and Notices --}}
+                <div>
+                    <h2 class="mb-4 text-md font-semibold text-gray-900 uppercase dark:text-white">Legal and Notices</h2>
+                    <ul class="text-gray-900 dark:text-gray-400 font-medium">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">Terms and Conditions</a>
+                        </li>
+                    </ul>
+                </div>
+                {{-- Social Media --}}
+                <div>
+                    <h2 class="mb-4 text-md font-semibold text-gray-900 uppercase dark:text-white">Social Media</h2>
+                    <ul class="text-gray-900 dark:text-gray-400 font-medium">
+                        <li class="mb-4">
+                            <a href="#" class="hover:underline">Github</a>
+                        </li>
+                        <li>
+                            <a href="#" class="hover:underline">X(Twitter)</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        {{-- Copy Right --}}
+        <hr class="my-6 border-gray-900 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+            <span class="text-sm text-gray-900 sm:text-center dark:text-gray-400">© 2024 <a href="#" class="hover:underline">EnviroStore</a>. All Rights Reserved.</span>
+        </div>
     </div>
 </footer>
