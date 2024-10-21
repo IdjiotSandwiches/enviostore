d1524c4697f5e64837f6236723d1ca485b6566d4
appleaze <theo.harjanto@binus.ac.id>
2024-10-21 11:01:48 +0700

    added card component

---
+++ resources/views/component/card.blade.php
@@ -0,0 +1,24 @@
+<div class="w-full max-w-72 bg-white border border-gray-200 rounded-lg shadow">
+    <a href="#">
+        <img class="rounded-t-lg" src="{{ asset('img/1.png') }}" alt="product image" />
+    </a>
+    <div class="pt-2 px-5 pb-5">
+        <a href="#">
+            <h5 class="text-2xl font-semibold tracking-tight text-font_primary dark:text-white">Product Eco Friendly 1</h5>
+        </a>
+        <a href="#">
+            <h5 class="text-base font-regular tracking-tight text-font_secondary dark:text-white">Seller Name</h5>
+        </a>
+        <div class="flex items-center mt-2.5 mb-2.5">
+            <div class="flex items-center space-x-1 rtl:space-x-reverse">
+                <svg class="w-5 h-5 text-white stroke-accent" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
+                </svg>
+            </div>
+            <span class= "text-base font-regular pl-2 py-0.75 text-font_secondary">4.5 | 25 Reviews</span>
+        </div>
+        <div class="flex items-center justify-between">
+            <span class="text-3xl font-bold text-font_primary dark:text-white">Rp. 5000</span>
+        </div>
+    </div>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,12 @@
 @section('title', 'Welcome')
 
 @section('content')
-
+<div class="container mx-auto px-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        @include('component.card')
+        @include('component.card')
+        @include('component.card')
+        @include('component.card')
+    </div>
+</div>
 @endsection
