5cdedd6fe55b01bbf81b7d30518ddca841e2fc83
Vincen <vincenhayate123@gmail.com>
2024-10-22 12:16:53 +0700

    Fix branch

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -10,7 +10,7 @@ $imgPaths = [
 @endphp
 
 @section('content')
-<section class="max-w-screen-xl md:px-4 md:py-8 md:mx-auto">
+<section class="max-w-screen-xl md:px-4 py-8 md:mx-auto">
     <section class="md:grid md:grid-cols-2 gap-4">
         <div class="glide group w-full md:w-80 lg:w-[24rem] xl:w-[30rem] md:h-[26rem] lg:h-[30rem] xl:h-[36rem]">
             <div class="glide__track" data-glide-el="track">
@@ -79,7 +79,7 @@ $imgPaths = [
     <section class="p-4 md:p-0 md:pt-10">
         <div id="accordion-collapse" data-accordion="collapse" data-active-classes="text-font_primary" data-inactive-classes="text-font_primary">
             <h2 id="accordion-collapse-heading-1">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-bold rtl:text-right border border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-bold rtl:text-right text-font_primary border-b border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 bg-primary hover:bg-accent/10 dark:hover:bg-accent/10 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
                     <span>About this Products</span>
                     <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
@@ -87,7 +87,7 @@ $imgPaths = [
                 </button>
             </h2>
             <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-                <div class="p-5 border rounded-b-xl border-gray-200 dark:border-gray-700 dark:bg-gray-900">
+                <div class="p-5 border border-t-0 rounded-b-xl bg-primary dark:bg-primary">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium et, magnam nulla quidem, modi voluptate ad temporibus vitae maxime alias sint praesentium delectus molestiae sunt accusantium, suscipit voluptatem mollitia.</p>
                 </div>
             </div>
