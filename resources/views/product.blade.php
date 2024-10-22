b30e53da855e15c3a0c25696209fc736b1c2e91e
Vincen <vincenhayate123@gmail.com>
2024-10-22 12:07:19 +0700

    Fix accordion active-inactive css class

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -77,9 +77,9 @@ $imgPaths = [
         </div>
     </section>
     <section class="p-4 md:p-0">
-        <div id="accordion-collapse" data-accordion="collapse">
+        <div id="accordion-collapse" data-accordion="collapse" data-active-classes="text-font_primary" data-inactive-classes="text-font_primary">
             <h2 id="accordion-collapse-heading-1">
-                <button type="button" class="flex items-center justify-between w-full p-5 font-bold rtl:text-right text-font_primary border border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
+                <button type="button" class="flex items-center justify-between w-full p-5 font-bold rtl:text-right border border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
                     <span>About this Products</span>
                     <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
