6a51fde95dcb0cae39ef7751f2cb577556524813
appleaze <theo.harjanto@binus.ac.id>
2024-10-23 12:13:02 +0700

    fix category tiles

--- resources/views/component/tiles.blade.php
+++ resources/views/component/tiles.blade.php
@@ -1,6 +1,6 @@
 <figure class="relative max-w-none max-h-full">
     <a href="#">
-        <img class="rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" alt="image description">
+        <img class="rounded-lg" src="{{ 'img/0.png' }}" alt="image description">
     </a>
     <figcaption class="absolute text-xl font-secondary left-1/2 bottom-2 transform -translate-x-1/2">
         <p class="bg-white rounded-full p-4 justify-center">Clothing</p>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,21 +1,22 @@
 @extends('layout.layout')
 @section('title', 'Welcome')
+@php
+$imagePaths=[
+    '/img/0.png',
+    '/img/1.png',
+    '/img/2.png',
+    '/img/3.png',
+    '/img/4.png',
+];
+@endphp
 
 @section('content')
-    @php
-        $imagePaths=[
-            '/img/0.png',
-            '/img/1.png',
-            '/img/2.png',
-            '/img/3.png',
-            '/img/4.png',
-        ];
-    @endphp
-
 @include('component.carousel', ['imgPaths' => $imagePaths])
-<h1 class="flex justify-center p-9 text-5xl font-secondary">
-    Category 
-</h1>
+<div class="flex justify-center p-9">
+    <h1 class="text-5xl font-secondary">
+        Category 
+    </h1>
+</div>
 <div class="container mx-auto px-4">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
         @include('component.tiles')
@@ -24,9 +25,11 @@
         @include('component.tiles')
     </div>
 </div>
-<h1 class="flex justify-center p-9 text-5xl font-secondary">
-    Recommended Product 
-</h1>
+<div class="flex justify-center p-9">
+    <h1 class="text-5xl font-secondary">
+        Recommended Products 
+    </h1>
+</div>
 <div class="container mx-auto px-4 pb-9">
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
         @include('component.card')
