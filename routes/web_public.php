83aefb4fd674a2753fc12d576010ddc482c43021
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-10-30 14:19:31 +0700

    Product Page (#32)
    * Product Page FE (#26)
    
    * Add product carousel
    
    * Product carousel thumbnail fix
    
    * Fix next prev btn
    
    * Fix next prev button & active state of thumbnail
    
    * Add accordion
    
    * Add dummy text
    
    * Adjust padding
    
    * Change corner round
    
    * Change carousel animation duration
    
    * Change accordion border & bg
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    * Mounting Google Drive as Cloud Storage (#25)
    
    * Setup GDrive connection
    
    * Create GoogleDriveUtility
    
    * Test image upload & retrieval
    
    * Rename comment
    
    * Add delete function
    
    * Add documentor
    
    * Notice comment
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    * Fix product page responsive (#27)
    
    * Add product carousel
    
    * Product carousel thumbnail fix
    
    * Fix next prev btn
    
    * Fix next prev button & active state of thumbnail
    
    * Add accordion
    
    * Add dummy text
    
    * Adjust padding
    
    * Change corner round
    
    * Change carousel animation duration
    
    * Fix carousel img aspect ratio
    
    * Fix Accordion placement
    
    * Fix accordion active-inactive css class
    
    * Add padding to accordion
    
    * Fix branch
    
    * Change from object-cover to object-contain
    
    * Remove padding
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    * Product Page Backend (#30)
    
    * Product, category, and image model & migration (#29)
    
    * Product, Categories, Images tables migration
    
    * Create model related to product & the relation of it
    
    * Change products table foreign key constraint datatype
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    * Create controller for product page & modify google_drive img retrieval
    
    * Fix product slider img size
    
    * Add encoding to route
    
    * Add notes
    
    * Move Utilities folder
    
    * Add product stocks in FE
    
    * Remove encode (need to be discuss)
    
    * Change database conf
    
    * Remove unnecessary files
    
    * Remove unnecessary line
    
    * Add pagination
    
    * Customize pagination
    
    * Create category interface & product utility
    
    * Move getProduct function to ProductUtility
    
    * Add SortDirectionInterface & modify ProductUtility to support sort
    
    * Change datatype
    
    * Fix sort mechanism in backend
    
    * Minor changes
    
    * Remove unnecessary
    
    * Remove pagination vendor
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>
    
    ---------
    
    Co-authored-by: Vincen <vincenhayate123@gmail.com>

--- routes/web_public.php
+++ routes/web_public.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\GoogleDriveController;
 use App\Http\Controllers\EmailVerificationController;
 
 /*
@@ -19,6 +21,10 @@ use App\Http\Controllers\EmailVerificationController;
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+Route::controller(ProductController::class)->group(function () {
+    Route::get('/products/{id}', 'getProduct')->name('getProduct');
+});
 
 Route::middleware(['guest:web,admin'])->group(function () {
     Route::controller(LoginController::class)->group(function () {
