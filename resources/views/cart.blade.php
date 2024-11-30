2fb95f887e6f842555a4ace4675078f24ef67c2d
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-11-30 18:16:32 +0700

    Add route & controller

---
+++ resources/views/cart.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.layout')
+@section('title', 'Cart')
+
+@section('content')
+
+@endsection
--- routes/web_auth.php
+++ routes/web_auth.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,3 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "auth" middleware group. Make something great!
 |
 */
+
+Route::controller(CartController::class)->group(function () {
+    Route::get('/cart', 'index')->name('cart');
+});
+
