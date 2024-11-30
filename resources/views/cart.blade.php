73811f9a2f851fd2bd06d80af5d328363d04dcec
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-11-30 18:44:00 +0700

    Add heading to page

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,5 +2,8 @@
 @section('title', 'Cart')
 
 @section('content')
-
+<section class="max-w-screen-xl px-4 py-8 md:mx-auto grid gap-4">
+    <h1 class="font-bold text-3xl">Your Cart</h1>
+    
+</section>
 @endsection
--- routes/web_auth.php
+++ routes/web_auth.php
@@ -17,4 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::controller(CartController::class)->group(function () {
     Route::get('/cart', 'index')->name('cart');
 });
-
