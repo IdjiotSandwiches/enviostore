30ac36a3ab24e47186b48a3d073edad8e628946d
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-11-30 21:30:23 +0700

    Create skeleton for items

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -4,6 +4,8 @@
 @section('content')
 <section class="max-w-screen-xl px-4 py-8 md:mx-auto grid gap-4">
     <h1 class="font-bold text-3xl">Your Cart</h1>
-    
+    <div>
+        @include('component.skeleton-item')
+    </div>
 </section>
 @endsection
---
+++ resources/views/component/skeleton-item.blade.php
@@ -0,0 +1,22 @@
+<div role="status" class="w-full h-full flex items-center bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 animate-pulse">
+    <div class="flex justify-center items-center rounded-t-lg aspect-square object-contain bg-gray-200 h-full">
+        <svg class="h-10 w-10 text-gray-300 dark:text-gray-600" aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+            <path
+                d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+        </svg>
+    </div>
+    <div class="p-5 flex flex-col gap-2 w-full">
+        <div class="">
+            <div class="flex justify-between">
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mb-4"></div>
+                <div class="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 mb-4"></div>
+            </div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-1/4 mb-2.5"></div>
+            <div class="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-1/4 mb-2.5"></div>
+        </div>
+        <div class="flex justify-end">
+            <div class="h-6 bg-gray-200 rounded-full dark:bg-gray-700 w-1/3 md:w-1/5"></div>
+        </div>
+    </div>
+</div>
