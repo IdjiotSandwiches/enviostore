6142d136052ac39a37eebf5171d25933298d6b8a
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-01 13:39:33 +0700

    Minor fix

--- resources/views/component/skeleton-item.blade.php
+++ resources/views/component/skeleton-item.blade.php
@@ -1,6 +1,6 @@
-<div role="status" class="w-full h-full flex items-center bg-white border-gray-200 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 animate-pulse">
+<div role="status" class="w-full h-full flex items-center bg-white border-gray-200 rounded-lg dark:bg-gray-800 animate-pulse">
     <div class="flex justify-center items-center rounded-t-lg aspect-square object-contain bg-gray-200 h-full">
-        <svg class="h-10 w-10 text-gray-300 dark:text-gray-600" aria-hidden="true"
+        <svg class="h-8 w-8 text-gray-300 dark:text-gray-600" aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
             <path
                 d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
