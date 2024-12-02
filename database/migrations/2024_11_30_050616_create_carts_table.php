fec59869adc29593ad8ceedb3d866b89dbb87f55
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-02 11:48:32 +0700

    Remigrate table

--- database/migrations/2024_11_30_050616_create_carts_table.php
+++ database/migrations/2024_11_30_050616_create_carts_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->integer('quantity')->unsigned();
-            $table->double('total_price')->unsigned();
             $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('product_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
