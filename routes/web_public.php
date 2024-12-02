22a3cf54273e680b8f8f766c5059b157ac581d50
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-02 20:01:38 +0700

    Move ProductController

--- routes/web_public.php
+++ routes/web_public.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\common\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\EmailVerificationController;
