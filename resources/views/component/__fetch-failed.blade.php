fa81a359bfd9c5d2b1c37e97dad3bfb4beb96433
Vin <124791055+IdjiotSandwiches@users.noreply.github.com>
2024-12-06 21:14:37 +0700

    Add fetch failed component (#51)
    * Add fetch failed component
    
    * Add fetch failed in category

---
+++ resources/views/component/__fetch-failed.blade.php
@@ -0,0 +1,12 @@
+<div class="w-full flex flex-col items-center">
+    <svg class="w-40 h-40 text-red-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+        height="24" fill="none" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1"
+            d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+    <h4 class="font-semibold text-2xl">An error has occured.</h4>
+    <a href="{{ route('home') }}"
+        class="mt-4 text-white bg-button disabled:cursor-not-allowed disabled:bg-button/70 hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-button/15 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-button dark:hover:bg-button/80 dark:focus:ring-button/15">
+        Return to homepage
+    </a>
+</div>
