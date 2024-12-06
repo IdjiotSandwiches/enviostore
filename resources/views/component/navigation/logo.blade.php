a105efaefc8a277bf3344ccd88f90142ef67bb21
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-06 22:10:01 +0700

    Rename components

--- resources/views/component/navigation/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<a href="{{ auth('admin')->check() ? route('admin.dashboard') : route('home') }}"
-    class="flex items-center space-x-3 rtl:space-x-reverse">
-    <span
-        class="self-center text-xl md:text-2xl font-semibold whitespace-nowrap dark:text-white italic">EnviroStore</span>
-</a>
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-@include('component.head')
+@include('component.__head')
 <body class="bg-background font-primary">
     @include('component.navigation.navbar')
     @yield('content')
-    @include('component.footer')
+    @include('component.__footer')
     @include('component.common-js')
     @yield('extra-js')
 </body>
