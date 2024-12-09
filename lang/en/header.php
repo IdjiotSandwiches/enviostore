679e146b99700db116fd936b0aa0e8c8c40cedda
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-09 16:23:43 +0700

    Publish and testing language

---
+++ lang/en/header.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'category' => 'Category',
+    'recommended' => 'Recommended Product',
+];
---
+++ lang/id/header.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'category' => 'Kategori',
+    'recommended' => 'Rekomendasi Produk',
+];
