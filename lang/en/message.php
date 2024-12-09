ca95b9029c15b78ba6a5c2082deb4d29a362757e
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-09 21:57:41 +0700

    Add language for error

--- lang/en/message.php
+++ lang/en/message.php
@@ -5,4 +5,7 @@ return [
     'logout' => 'Logged Out',
     'email_password_error' => 'Email or password invalid.',
     'invalid' => 'Invalid operation.',
+    'fetch_error' => 'An error has occured.',
+    'return_home' => 'Return to Homepage',
+    'not_found' => 'Hey, what are you looking for?',
 ];
--- lang/id/message.php
+++ lang/id/message.php
@@ -4,5 +4,8 @@ return [
     'login' => 'Berhasil Masuk.',
     'logout' => 'Berhasil Keluar.',
     'email_password_error' => 'Email atau password salah.',
-    'invalid' => 'Terjadi Kesalahan.',
+    'invalid' => 'Operasi tidak valid.',
+    'fetch_error' => 'Terjadi kesalahan.',
+    'return_home' => 'Kembali ke Beranda',
+    'not_found' => 'Hai, apa yang sedang anda cari?',
 ];
