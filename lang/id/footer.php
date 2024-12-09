11a049e4ac180c8ca11ebe851b90972a738924eb
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-09 17:03:16 +0700

    Add localization in footer

---
+++ lang/id/footer.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'resources' => 'Sumebr',
+    'resources.goals' => 'Tujuan Pembangunan Berkelanjutan',
+    'resouces.supply' => 'Transparansi Pasokan',
+    'legal' => 'Hukum dan Terkait',
+    'legal.privacy' => 'Kebijakan Privasi',
+    'legal.terms' => 'Syarat dan Ketentuan',
+    'social' => 'Media Sosial',
+    'rights' => 'Seluruh Hak Cipta Dilindungi Undang-Undang',
+];
