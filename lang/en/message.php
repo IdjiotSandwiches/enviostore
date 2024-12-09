4e4907a4c165bd525391784d491c0b2cba09cc12
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-09 21:31:48 +0700

    Add language for login

---
+++ lang/en/message.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'login' => 'Logged In.',
+    'logout' => 'Logged Out',
+    'email_password_error' => 'Email or password invalid.',
+];
---
+++ lang/id/message.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'login' => 'Berhasil Masuk.',
+    'logout' => 'Berhasil Keluar.',
+    'email_password_error' => 'Email atau password salah.',
+];
