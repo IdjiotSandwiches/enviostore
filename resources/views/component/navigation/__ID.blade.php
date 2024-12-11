dc2c8c77a834f225a9ea1b20f17a13a296bfd60b
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-11 12:13:25 +0700

    Change toggle to dropdown

---
+++ resources/views/component/navigation/__ID.blade.php
@@ -0,0 +1,7 @@
+<p class="flex items-center gap-2">
+    <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 32 32">
+        <path d="M35,8H1V18H28V8H5Z" fill="#ea3323"></path>
+        <path d="M5,28H27.5V18H1v10H5Z" fill="#fff"></path>
+    </svg>
+    ID
+</p>
--- resources/views/component/navigation/__toggle.blade.php
+++ resources/views/component/navigation/__toggle.blade.php
@@ -1,35 +1,27 @@
-<form action="{{ route('toggleLanguage', app()->getLocale() === 'en' ? 'id' : 'en') }}" method="GET" class="flex">
-    @csrf
-    @method('GET')
-    <label class="inline-flex gap-2 items-center cursor-pointer">
-        <input type="checkbox" onchange="this.form.submit()" class="sr-only peer" {{ app()->getLocale() === 'id' ? 'checked' : '' }}>
-        <p class="flex items-center gap-2">
-            <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 30" width="5" height="5">
-                <clipPath id="s">
-                    <path d="M0,0 v30 h60 v-30 z" />
-                </clipPath>
-                <clipPath id="t">
-                    <path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z" />
-                </clipPath>
-                <g clip-path="url(#s)">
-                    <path d="M0,0 v30 h60 v-30 z" fill="#012169" />
-                    <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" stroke-width="6" />
-                    <path d="M0,0 L60,30 M60,0 L0,30" clip-path="url(#t)" stroke="#C8102E" stroke-width="4" />
-                    <path d="M30,0 v30 M0,15 h60" stroke="#fff" stroke-width="10" />
-                    <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" stroke-width="6" />
-                </g>
-            </svg>
-            EN
-        </p>
-        <div
-            class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-black/10 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all peer-checked:bg-black">
-        </div>
-        <p class="flex items-center gap-2">
-            <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 32 32">
-                <path d="M35,8H1V18H28V8H5Z" fill="#ea3323"></path>
-                <path d="M5,28H27V20H1v12H5Z" fill="#fff"></path>
-            </svg>
-            ID
-        </p>
-    </label>
-</form>
+<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
+    class="text-white bg-button hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-bg-button/20 font-medium rounded-lg text-sm px-2.5 py-1.5 text-center inline-flex items-center"
+    type="button">
+    @if (app()->getLocale() === 'id')
+        @include('component.navigation.__ID')
+    @elseif (app()->getLocale() === 'en')
+        @include('component.navigation.__EN')
+    @endif
+    <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+    </svg>
+</button>
+
+<div id="dropdown" class="z-10 hidden bg-background divide-y divide-gray-100 rounded-lg shadow">
+    <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
+        <li class="block px-5 py-2.5  hover:bg-gray-100">
+            <a href="{{ route('toggleLanguage', 'id') }}">
+                @include('component.navigation.__ID')
+            </a>
+        </li>
+        <li class="block px-5 py-2.5  hover:bg-gray-100">
+            <a href="{{ route('toggleLanguage', 'en') }}">
+                @include('component.navigation.__EN')
+            </a>
+        </li>
+    </ul>
+</div>
