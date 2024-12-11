317fd1f8f196c09613a627b1e555e4e165dbf281
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-11 12:35:06 +0700

    Fix ID flag

--- resources/views/component/navigation/__ID.blade.php
+++ resources/views/component/navigation/__ID.blade.php
@@ -1,7 +1,7 @@
 <p class="flex items-center gap-2">
     <svg class="w-8 h-8" xmlns="http://www.w3.org/2000/svg" width="5" height="5" viewBox="0 0 32 32">
-        <path d="M35,8H1V18H28V8H5Z" fill="#ea3323"></path>
-        <path d="M5,28H27.5V18H1v10H5Z" fill="#fff"></path>
+        <path d="M32,5H0V17H28V5H5Z" fill="#ea3323"></path>
+        <path d="M5,28H0V17H28v11H5Z" fill="#fff"></path>
     </svg>
     ID
 </p>
--- resources/views/component/navigation/__toggle.blade.php
+++ resources/views/component/navigation/__toggle.blade.php
@@ -1,9 +1,9 @@
 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown"
-    class="text-white bg-button hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-bg-button/20 font-medium rounded-lg text-sm px-2.5 py-1.5 text-center inline-flex items-center"
+    class="text-white bg-button hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-bg-button/20 font-medium rounded-lg text-sm px-2.5 py-1 text-center inline-flex items-center"
     type="button">
-    @if (app()->getLocale() === 'id')
+    @if (\Illuminate\Support\Facades\App::getLocale() === 'id')
         @include('component.navigation.__ID')
-    @elseif (app()->getLocale() === 'en')
+    @elseif (\Illuminate\Support\Facades\App::getLocale() === 'en')
         @include('component.navigation.__EN')
     @endif
     <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -13,12 +13,12 @@
 
 <div id="dropdown" class="z-10 hidden bg-background divide-y divide-gray-100 rounded-lg shadow">
     <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownDefaultButton">
-        <li class="block px-5 py-2.5  hover:bg-gray-100">
+        <li class="block px-5 py-2.5 hover:bg-gray-100">
             <a href="{{ route('toggleLanguage', 'id') }}">
                 @include('component.navigation.__ID')
             </a>
         </li>
-        <li class="block px-5 py-2.5  hover:bg-gray-100">
+        <li class="block px-5 py-2.5 hover:bg-gray-100">
             <a href="{{ route('toggleLanguage', 'en') }}">
                 @include('component.navigation.__EN')
             </a>
