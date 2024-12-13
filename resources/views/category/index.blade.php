ab65281b8ed35ef80ba8447fe5c2a99af6b340a2
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-13 12:30:36 +0700

    Fix lang in category

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,24 +1,24 @@
 @extends('layout.layout')
-@section('title', $category->name)
+@section('title', ucwords($category->name))
 
 @section('content')
 <section class="max-w-screen-xl px-4 py-8 md:mx-auto grid gap-4">
     <h1 class="font-bold text-3xl">{{ ucfirst($category->name) }}</h1>
-    <button id="filterDropdown" data-dropdown-toggle="dropdown" class="flex gap-2 p-2 rounded-lg max-w-[18rem] justify-center items-center bg-primary text-font_primary border border-font_primary" type="button">
-        {{ __('category.sort') }}:
+    <button id="filterDropdown" data-dropdown-toggle="filterDropdownItems" class="flex gap-2 p-2 rounded-lg max-w-[18rem] justify-center items-center bg-primary text-font_primary border border-font_primary" type="button">
+        {{ __('page.category.sort') }}:
         <span class="font-bold">
-            {{ __('category.newest') }}
+            {{ __('page.category.newest') }}
         </span>
         <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6h-2m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4"/>
         </svg>
     </button>
 
-    <div id="dropdown" class="max-w-xs z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-72 dark:bg-gray-700">
+    <div id="filterDropdownItems" class="max-w-xs z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-72 dark:bg-gray-700">
         <ul class="py-2 text-gray-700 dark:text-gray-200" aria-labelledby="filterDropdown">
-            <li value="1" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('category.newest') }}</li>
-            <li value="2" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('category.lowest') }}</li>
-            <li value="3" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('category.highest') }}</li>
+            <li value="1" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('page.category.newest') }}</li>
+            <li value="2" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('page.category.lowest') }}</li>
+            <li value="3" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('page.category.highest') }}</li>
         </ul>
     </div>
 
