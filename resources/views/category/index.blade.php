9e2f4f70e3e109dba982d3046919b529296ef6eb
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2024-12-15 00:33:46 +0700

    Remove dark: tag on css class

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -9,16 +9,16 @@
         <span class="font-bold">
             {{ __('page.category.newest') }}
         </span>
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+        <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6h-2m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4m16 6H10m0 0a2 2 0 1 0-4 0m4 0a2 2 0 1 1-4 0m0 0H4"/>
         </svg>
     </button>
 
-    <div id="filterDropdownItems" class="max-w-xs z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-72 dark:bg-gray-700 cursor-pointer">
-        <ul class="py-2 text-gray-700 dark:text-gray-200" aria-labelledby="filterDropdown">
-            <li value="1" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('page.category.newest') }}</li>
-            <li value="2" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('page.category.lowest') }}</li>
-            <li value="3" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{{ __('page.category.highest') }}</li>
+    <div id="filterDropdownItems" class="max-w-xs z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-72  cursor-pointer">
+        <ul class="py-2 text-gray-700 " aria-labelledby="filterDropdown">
+            <li value="1" class="block px-4 py-2 hover:bg-gray-100">{{ __('page.category.newest') }}</li>
+            <li value="2" class="block px-4 py-2 hover:bg-gray-100">{{ __('page.category.lowest') }}</li>
+            <li value="3" class="block px-4 py-2 hover:bg-gray-100">{{ __('page.category.highest') }}</li>
         </ul>
     </div>
 
--- resources/views/component/navigation/drawer/__drawer.blade.php
+++ resources/views/component/navigation/drawer/__drawer.blade.php
@@ -1,7 +1,7 @@
 <div class="flex items-center">
     <button type="button" data-drawer-target="drawer" data-drawer-show="drawer" data-drawer-backdrop="false"
         aria-controls="drawer" aria-controls="drawer">
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+        <svg class="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
             width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h14" />
         </svg>
@@ -9,11 +9,11 @@
 </div>
 
 <div id="drawer"
-    class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-primary w-full dark:bg-gray-800"
+    class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-primary w-full"
     tabindex="-1" aria-labelledby="drawer-label">
     <h3 class="uppercase font-semibold">{{ __('settings') }}</h3>
     <button type="button" data-drawer-hide="drawer" aria-controls="drawer"
-        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white">
+        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center">
         <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                 d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
