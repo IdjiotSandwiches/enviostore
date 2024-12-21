23110e42a5b554de3d0d122e31a5167d8f6b530f
appleaze <theo.harjanto@binus.ac.id>
2024-12-21 23:51:42 +0700

    added skeleton js

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -21,7 +21,7 @@
             </h1>
         </div>
         <div class="mx-auto px-4 pb-9">
-            <div class="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-3">
+            <div id="productContainer" class="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-3">
                 @foreach ($products as $product)
                     @include('component.__product-card', [
                         'link' => $product->link,
@@ -34,31 +34,91 @@
             </div>
         </div>
     </div>
-</div>
+    </div>
 @endsection
 
 @section('extra-js')
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const carousel = new Glide('.glide_carousel', {
-            type: 'carousel',
-            startAt: 0,
-            autoplay: 3000,
-            animationDuration: 1000,
-            hoverpause: true,
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const carousel = new Glide('.glide_carousel', {
+                type: 'carousel',
+                startAt: 0,
+                autoplay: 3000,
+                animationDuration: 1000,
+                hoverpause: true,
+            });
+
+            const slider = new Glide('.glide_slider', {
+                type: 'slider',
+                startAt: 0,
+                animationDuration: 350,
+                perView: 4,
+                bound: true,
+                rewind: false,
+            });
+
+            carousel.mount({
+                Controls,
+                Breakpoints,
+                Swipe,
+                Autoplay
+            });
+            slider.mount({
+                Controls,
+                Breakpoints,
+                Swipe
+            });
         });
 
-        const slider = new Glide('.glide_slider', {
-            type: 'slider',
-            startAt: 0,
-            animationDuration: 350,
-            perView: 4,
-            bound: true,
-            rewind: false,
-        });
+        function fetchRequest(url) {
+            let productContainer = document.querySelector('#productContainer');
+            productContainer.replaceChildren();
 
-        carousel.mount({ Controls, Breakpoints, Swipe, Autoplay });
-        slider.mount({ Controls, Breakpoints, Swipe });
-    });
-</script>
+            setTimeout(function() {
+                if (productContainer.textContent !== '') return;
+
+                for (let i = 0; i < 8; i++) {
+                    let item = `{!! view('component.__skeleton-card')->render() !!}`;
+                    productContainer.insertAdjacentHTML('beforeend', item);
+                }
+            }, 200);
+
+            fetch(url, {
+                method: 'GET',
+            }).then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                productContainer.replaceChildren();
+                return response.json();
+            }).then(products => {
+                products.forEach(product => {
+                    let productCard = `
+                @include('component.__product-card', [
+                    'link' => '${product.link}',
+                    'image' => '${product.img}',
+                    'name' => '${product.name}',
+                    'rating' => ${product . rating},
+                    'price' => '${product.price}',
+                ])
+            `;
+                    productContainer.insertAdjacentHTML('beforeend', productCard);
+                });
+            }).catch(error => {
+                let section = document.querySelector('section');
+                let item = `{!! view('component.__fetch-failed')->render() !!}`;
+
+                section.replaceChildren();
+                section.insertAdjacentHTML('beforeend', item);
+            });
+
+            function retryFetch() {
+                loadProducts();
+            }
+
+            document.addEventListener('DOMContentLoaded', () => {
+                loadProducts();
+            });
+        }
+    </script>
 @endsection
