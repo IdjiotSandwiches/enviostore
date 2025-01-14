d2e5c359cde52bc6ec2c607cce2aab1d0d1e6614
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-01-14 17:17:48 +0700

    Minor fix

--- resources/views/profile/component/__order-item.blade.php
+++ resources/views/profile/component/__order-item.blade.php
@@ -7,7 +7,8 @@
     </div>
     @if (!collect([
         \App\Interfaces\PaymentStatusInterface::STATUS_COMPLETE, 
-        \App\Interfaces\PaymentStatusInterface::STATUS_CANCEL])->contains($order->payment_status)
+        \App\Interfaces\PaymentStatusInterface::STATUS_CANCEL
+        ])->contains($order->payment_status)
     )
         <a href="{{ route('checkout.payment', $order->order_id) }}" class="text-white bg-button disabled:cursor-not-allowed disabled:bg-button/70 hover:bg-button/80 focus:ring-4 focus:outline-none focus:ring-button/15 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Pay</a>
     @endif
