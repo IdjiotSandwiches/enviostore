63d863210004155815f2f4ea38ee2f91e319c0e6
IdjiotSandwiches <idjiotsandwiches@gmail.com>
2025-01-19 21:55:18 +0700

    Remove imports

--- routes/web_admin.php
+++ routes/web_admin.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProfilePictureController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\admin\AdminController;
 use App\Http\Controllers\admin\ProductController;
 use App\Http\Controllers\admin\CategoryController;
 
